35aa1e692e5fa3d13c2230cf9f4bcd060307cd47
zipang29 <guillaume.claudic@gmail.com>
2012-11-13 19:41:49 +0100

    resuite

--- src/Mandataire.java
+++ src/Mandataire.java
@@ -28,17 +28,24 @@ public class Mandataire {
 		System.out.println("[~] socket du serveur de réception : " + this.serveurSocketReception);
 		
 		this.receptionIn = new Vector<DataInputStream>();
-		this.envoi = new Vector<DataOutputStream>();
+		this.receptionOut = new Vector<DataOutputStream>();
+		this.envoiOut = new Vector<DataOutputStream>();
+		this.envoiIn = new Vector<DataInputStream>();
 		
 		System.out.println("[*] Le serveur est démarré.");
 		while(true) {
 			// On accepte la connexion
 			Socket receptionSocket = this.serveurSocketReception.accept();
 			System.out.println( "[*] Nouvelle connexion entrante depuis : " + receptionSocket.getRemoteSocketAddress());
-			//On récupère le IN et le OUT du client
+			//On récupère le IN et le OUT du vrai client
 			DataInputStream inReception = new DataInputStream(receptionSocket.getInputStream());
 			DataOutputStream outReception = new DataOutputStream(receptionSocket.getOutputStream());
 			receptionIn.add(inReception);
+			receptionOut.add(outReception);
+			
+			//On récupère le IN et le OUT du vrai serveur
+			DataInputStream inEnvoi = new DataInputStream();
+			DataOutputStream outEnvoi = new DataOutputStream();
 			//On se connecte en tant que client au serveur demandé
 			Socket envoiSocket = new Socket(adresse, portServeur);
 			System.out.println("[*] Nouvelle connexion sortante vers le serveur " + adresse);
