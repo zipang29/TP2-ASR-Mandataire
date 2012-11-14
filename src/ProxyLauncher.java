d0afb09878312d598dea747302a928f3e02c9dca
GUITTET Thibault <alan-mushi@yahoo.com>
2012-11-14 11:06:23 +0100

    modification du lanceur de Proxy

--- src/ProxyLauncher.java
+++ src/ProxyLauncher.java
@@ -2,25 +2,30 @@ public class ProxyLauncher {
 
 	public static void main( String[] args ) {
 		int port = 1100 ;
+		String chatIP = "localhost" ;
+		int chatPort = 1099 ;
 		if ( args.length != 1 ) { 
 			help() ; 
 		}
 		else {
 			try { 
-				port = Integer.valueOf( args[0] ) ; 
-
+				port = Integer.valueOf( args[0] ) ;
+				chatIP = args[1] ;
+				chatPort = Integer.valueOf( args[2] ) ;
 			}
 			catch ( NumberFormatException e ) {
-				System.out.println( "[-] Le n° de port n'a pas été correctement mis : " + e.getMessage() ) ;
+				System.out.println( "[-] Un des ports a été mal renseigné : " + e.getMessage() ) ;
 			}
 		}
-		new Proxy( port , "localhost" , 1099) ;
+		new Proxy( port , chatIP , chatPort ) ;
 		System.out.println( "[+] Serveur de chat lancé sur le port " + port ) ;
 	}
 
 	private static void help() {
-		String help = "\nUsage : java ProxyLauncher <port>\n" ;
-		help += "\nSi le port n'est pas défini 1100 sera attribué.\n\n" ;
+		String help = "\nUsage : java ProxyLauncher <port> <IP du serveur> <port du serveur>\n" ;
+		help += "\nSi le port n'est pas défini 1100 sera attribué.\n" ;
+		help += "Si l'ip du serveur n'est pas défini localhost sera attribué\n" ;
+		help += "Si le port du serveur n'est pas défini 1099 sera attribué\n" ;
 		System.out.println( help ) ;
 	}
 }
