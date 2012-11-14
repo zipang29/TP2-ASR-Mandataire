8f5d4822dd6b834c917dc31642109a7b6b818878
GUITTET Thibault <alan-mushi@yahoo.com>
2012-11-14 10:59:15 +0100

    gestion de la déconnexion

--- src/myThread.java
+++ src/myThread.java
@@ -15,13 +15,18 @@ public class myThread extends Thread {
 		try {
 			byte[] b = new byte[256] ;
 			int n = this.in.read( b ) ;
-			while ( n != 0 ) {
+			while ( n > 0 ) {
+				String msg = new String( b , 0 , n ) ;
+				System.out.println( "[~] message transmis : " + msg ) ;
 				this.out.write( b , 0 , n ) ;
 				this.out.flush() ;
-				System.out.println( "[~] message transmis : " + (new String( b )).toString()) ;
 				for ( int i = 0 ; i < b.length-1 ; i++ ) { b[i] = 0x00 ; }
 				n = this.in.read( b ) ;
 			}
+			this.in.close() ;
+			this.out.close() ;
+		} catch ( java.net.SocketException e ) {
+			System.out.println( "[*] Client déconnecté." ) ;
 		} catch ( java.io.IOException e ) {
 			e.printStackTrace() ;
 		}
